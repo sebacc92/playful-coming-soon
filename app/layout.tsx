696fa6a42be759e891cdfe09b9e1be52f94d9cad
Seba Cardoso <hi@sebastiancardoso.com>
2025-03-11 10:54:04 -0300

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Geounity App',
+  description: 'Cooming soon',
+  generator: 'https://sebastiancardoso.com',
 }
 
 export default function RootLayout({
